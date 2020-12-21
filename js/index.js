6e785477f0bbee246227aa1b6f2702df07991466
cuong nguyen <cuongpianna1996@gmail.com>
2020-12-21 07:59:56 +0700

    .

--- js/index.js
+++ js/index.js
@@ -1,32 +1,29 @@
 jQuery(document).ready(function (e) {
 
 
-    $('.scroll').click(function(e) {
-        e.preventDefault();
-        var target = $(this).attr('target');
-        $('html, body').animate({
-            scrollTop: ($(target).offset().top)
-        }, 1500);
-    });
+    window.onscroll = function () {
+        scrollFunction()
+    };
 
 
+    function scrollFunction() {
+        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+            $(".header").addClass('fixed');
+        } else {
+            $(".header").removeClass('fixed');
+        }
+    }
 
-    // window.onscroll = function () {
-    //     scrollFunction()
-    // };
-    //
-    // // khai báo hàm scrollFunction
-    // function scrollFunction() {
-    //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //         document.getElementById("scrollTop").style.display = "block";
-    //     } else {
-    //         document.getElementById("scrollTop").style.display = "none";
-    //     }
-    // }
-    //
-    // document.getElementById('scrollTop').addEventListener("click", function () {
-    //     $("html, body").animate({ scrollTop: 0 }, "slow");
-    // });
+    $('.mobile-open').click(function () {
+        $('.nav_wrap').toggle('fast')
+        if(!$('.header').hasClass('mobile_fixed')) {
+            $('.header').addClass('mobile_fixed');
+        }else {
+            $('.header').removeClass('mobile_fixed');
+        }
+        // $('.header').addClass('fixed');
+    })
+
 
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
@@ -35,8 +32,22 @@ jQuery(document).ready(function (e) {
             prevEl: '.swiper-button-prev',
         },
         speed: 400,
-        spaceBetween: 100,
-        slidesPerView: 6
+        // spaceBetween: 100,
+        slidesPerView: 6,
+        breakpoints: {
+            0: {
+                slidesPerView: 2,
+                spaceBetween: 150,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 100,
+            },
+            1024: {
+                slidesPerView: 6,
+                spaceBetween: 100,
+            },
+        }
     })
 
 });
