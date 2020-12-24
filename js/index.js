36eb594a4bfc999e4d61d88851f57bdae1a4a5ce
cuong <cuongpianna1996@gmail.com>
2020-12-24 18:12:53 +0700

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,39 @@
 jQuery(document).ready(function (e) {
-    $('.close').click(function () {
-        console.log('sss')
-        $('.nav-mobile').toggleClass('active');
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        navText : ['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
+        responsive:{
+            0:{
+                items:2
+            },
+            768:{
+                items:4
+            },
+            1024:{
+                items:5,
+                margin: 30
+            },
+            1100:{
+                items:6,
+                margin: 30
+            }
+        }
     })
+
+    $('.icon-mobile').click(function (){
+        $('.header-menu').slideToggle('fast');
+    })
+
+    $('.owl-carousel')
+        .mouseenter(function() {
+            $('.owl-prev').show(50);
+            $('.owl-next').show(50);
+        })
+        .mouseleave(function() {
+            $('.owl-prev').hide();
+            $('.owl-next').hide();
+        });
 });
