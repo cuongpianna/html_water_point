35433fa2f7729a28616ae3c24e176d69d73421ea
cuong <cuongpianna1996@gmail.com>
2021-02-19 18:54:03 +0700

    .

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function (e) {
-
+    $('.nav-button').click(function (){
+        if($(this).hasClass('close')) {
+            $(this).removeClass('close');
+        }else {
+            $(this).addClass('close');
+        }
+    })
 });
