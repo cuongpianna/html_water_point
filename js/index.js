37a376239ad614c3b297bf0543f3eaa1b1db9910
cuong nguyen <cuongpianna1996@gmail.com>
2021-02-20 07:52:29 +0700

    .

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ jQuery(document).ready(function (e) {
     $('.nav-button').click(function (){
         if($(this).hasClass('close')) {
             $(this).removeClass('close');
+            $('header .navigation').removeClass('mobile');
+            $('body').removeClass('no-scroll');
         }else {
             $(this).addClass('close');
+            $('header .navigation').addClass('mobile');
+            $('body').addClass('no-scroll');
         }
     })
 });
