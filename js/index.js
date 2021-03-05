7e8191a25c30d29f41575d752927bc92090142e4
cuong <cuongpianna1996@gmail.com>
2021-03-05 22:43:50 +0700

    .

--- js/index.js
+++ js/index.js
@@ -58,4 +58,50 @@ jQuery(document).ready(function (e) {
     }else if(link.includes('feedback')) {
         $('.feedback').addClass('active');
     }
+
+    let width = window.innerWidth;
+
+    // rule page
+    $('.rule-page .rule').click(function (){
+
+        $('.rule-content').hide();
+
+        $(`#${$(this).attr('data-rule')}`).show("slide", { direction: "right" }, 300);
+
+        if(width <= 768) {
+            $('.rule-page .content-right').addClass('show');
+
+            $('.rule-content h4').click(function (){
+                $('.rule-content').hide();
+                $('.rule-page .content-right').removeClass('show');
+            });
+
+            $('.rule-content h3').click(function (){
+                $('.rule-content').hide();
+                $('.rule-page .content-right').removeClass('show');
+            });
+        }
+    })
+
+    // map page
+    $('.map-page .content-folder__title').click(function (){
+        $('.map-content').hide();
+        $(`#${$(this).attr('data-map')}`).show("slide", { direction: "right" }, 300);
+
+        let widthh = window.innerWidth
+
+        if(widthh <= 768) {
+            $('.map-page .content-right').addClass('show');
+
+            // $('.rule-content h4').click(function (){
+            //     $('.rule-content').hide();
+            //     $('.rule-page .content-right').removeClass('show');
+            // });
+            //
+            // $('.rule-content h3').click(function (){
+            //     $('.rule-content').hide();
+            //     $('.rule-page .content-right').removeClass('show');
+            // });
+        }
+    })
 });
