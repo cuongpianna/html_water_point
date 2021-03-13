49dae477c31b455ab24a6f9711e461d6fd4c87d2
cuong <cuongpianna1996@gmail.com>
2021-03-13 14:58:24 +0700

    .

--- js/index.js
+++ js/index.js
@@ -21,6 +21,20 @@ jQuery(document).ready(function (e) {
         }else {
             $('.box-animate').removeClass('bg-gray');
         }
+        let width = $(window).width();
+        if(width > 768) {
+            let bannerH =  $('.introduce')[0].getBoundingClientRect().top
+            if(bannerH < 0) {
+                var $div = $('.navigation');
+                $('.navigation').addClass('fixed');
+                $('.navigation').animate({
+                    'right': '50px'
+                });
+
+            }else {
+                $('.navigation').removeClass('fixed');
+            }
+        }
     }
 
 
@@ -61,9 +75,48 @@ jQuery(document).ready(function (e) {
     });
 
 
-    console.log($('.swiper-wrapper'))
-
     $('.swiper-wrapper').attr('style', 'transform: translate3d(0px,-135px, 0px);')
 
+    $('.nav1').addClass('show');
+    var show = 'nav1';
+    setInterval(function (){
+        if(show === 'nav1') {
+            show = 'nav2';
+            $('.slick2').fadeIn(500);
+            $('.slick1').fadeOut(500);
+            $('.nav2').addClass('show');
+            $('.nav1').removeClass('show');
+            $('.slider-header').addClass('slide2');
+            $('.slider-header').removeClass('slide1');
+        }else {
+            show = 'nav1';
+            $('.slick1').fadeIn(500);
+            $('.slick2').fadeOut(500);
+            $('.nav1').addClass('show');
+            $('.nav2').removeClass('show');
+            $('.slider-header').addClass('slide1');
+            $('.slider-header').removeClass('slide2');
+        }
+    }, 5000)
+
+    $('.slider-header__nav .navs').click(function () {
+        if($(this).hasClass('nav1')) {
+            show = 'nav1';
+            $('.nav1').addClass('show');
+            $('.nav2').removeClass('show');
+            $('.slick1').fadeIn(100);
+            $('.slick2').fadeOut(100);
+            $('.slider-header').addClass('slide1');
+            $('.slider-header').removeClass('slide2');
+        }else {
+            show = 'nav2';
+            $('.nav2').addClass('show');
+            $('.nav1').removeClass('show');
+            $('.slick2').fadeIn(100);
+            $('.slick1').fadeOut(100);
+            $('.slider-header').addClass('slide2');
+            $('.slider-header').removeClass('slide1');
+        }
+    })
 });
 
